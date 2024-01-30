b5a5f41a667dde7f77643b7ac3b2b73120a6e7fb
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-01-30 18:22:37 +0530

    update admin panel

--- Asstes/files/control.js
+++ Asstes/files/control.js
@@ -1,7 +1,16 @@
-$("#login").css("display","block");
-$("#dashboard").css("display","none");
+$("#login").css("display","none");
+$("#dashboardone").css("display","block");
+$("#guide").css("display","none");
+$("#dash").css("display","block");
 
 document.getElementById("loginbtn").addEventListener('click',function (){
     $("#login").css("display","none");
-    $("#dashboard").css("display","block");
+    $("#dashboardone").css("display","block");
+});
+
+document.getElementById("btn-guide").addEventListener('click',function (){
+    $("#login").css("display","none");
+    $("#dashboardone").css("display","block");
+    $("#dash").css("display","none");
+    $("#guide").css("display","block");
 });
